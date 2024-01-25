fdcd1038f3590a3d6ab56b14f646612e2f2808ec
unknown <fran.millan95@gmail.com>
2024-01-25 18:16:23 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express")
+const {auth} = require("express-oauth2-jwt-bearer")
+const errorHandler = require("./middleware/errorHandler")
+
+
+// Configuracion del Middleware con el servidor de Autorizacion
+const autenticacion = auth({
+    audience: "http://localhost:3000/api/productos",
+    issuerBaseURL: "https://dev-utn-frc-iaew.auth0.com/",
+    tokenSigningAlg: "RS256"
+})
+
+const app = express()
+app.use(express.json())
+
+
+// importamos el router de libros
+const librosRouter = require("./router/libros")
+
+
+app.use("./libros",autenticacion, librosRouter)
+
+app.use(errorHandler)
+
+app.listen(3000,()=>{
+    console.log("Servidor iniciado en el puerto 3000")
+}) 
